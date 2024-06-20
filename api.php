3d4545fcf7462cf3ed56fc199914e1c557d5bea7
Dihep <dihep.demayo@gmail.com>
2024-06-20 22:17:48 +0200

    Stilizzazione.

--- api.php
+++ api.php
@@ -3,32 +3,32 @@
 $to_do = [
     [
         "task" => "Esempio di compito da svolgere #1",
-        "statoTask" => "To-Do",
+        "statoTask" => "to-do",
         "scadenza" => "1-1-2001"
     ],
     [
         "task" => "Esempio di compito da svolgere #2",
-        "statoTask" => "To-Do",
+        "statoTask" => "to-do",
         "scadenza" => "2-1-2001"
     ],
     [
         "task" => "Esempio di compito da svolgere #3",
-        "statoTask" => "To-Do",
+        "statoTask" => "to-do",
         "scadenza" => "3-1-2001"
     ],
     [
         "task" => "Esempio di compito da svolgere #4",
-        "statoTask" => "To-Do",
+        "statoTask" => "to-do",
         "scadenza" => "4-1-2001"
     ],
     [
         "task" => "Esempio di compito da svolgere #5",
-        "statoTask" => "To-Do",
+        "statoTask" => "to-do",
         "scadenza" => "5-1-2001"
     ],
     [
         "task" => "Esempio di compito da svolgere #6",
-        "statoTask" => "To-Do",
+        "statoTask" => "to-do",
         "scadenza" => "6-1-2001"
     ]
 ];
